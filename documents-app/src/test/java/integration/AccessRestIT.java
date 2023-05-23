436201c4b25baa933c99e4094bae9049673edaec
katevas <katya.vasilevskaya.01@mail.ru>
2023-05-23 23:28:49 +0300

    CI е2е tests check

--- documents-app/src/test/java/integration/AccessRestIT.java
+++ documents-app/src/test/java/integration/AccessRestIT.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class AccessRestIntegrationTestIT {
+public class AccessRestIT {
     @Autowired
     private TestRestTemplate template;
 
