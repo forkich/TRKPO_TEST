ab8dee2c37a198e2fcba21d3214bffb4a67688b1
katevas <katya.vasilevskaya.01@mail.ru>
2023-05-24 12:35:15 +0300

    CI е2е tests check

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -45,32 +45,33 @@ describe('Export users', () => {
   });
 
   it('should download the list of users after clicking EXPORT button', () => {
-      cy.visit(url);
+      enterCredentials('invalid-login', 'invalid-password')
   });
 });
 
 describe("Edit document type", () => {
   it("should edit a document type after clicking EDIT button and display the change", () => {
-      cy.visit(url);
+      enterCredentials('invalid-login', 'invalid-password')
   });
 });
 
 describe('Navigate from one page to another', () => {
   it('should navigate from main page to page Catalogues', () => {
-      cy.visit(url);
+     enterCredentials('invalid-login', 'invalid-password')
   })
 })
 
 
 describe('Add new catalogue', () => {
   it('should add new catalogue when user click on button CREATE', () => {
-     cy.visit(url);
+    enterCredentials('invalid-login', 'invalid-password')
   });
 });
 
 describe('Change user role', () => {
   it('should change the user role and display it', () => {
-      cy.visit(url);
+      enterCredentials('invalid-login', 'invalid-password')
+      enterCredentials('invalid-login', 'invalid-password')
 
   })
 })
@@ -85,43 +86,28 @@ describe('Export documents info', () => {
   });
 
   it('should download the list of all documents and read the downloaded file', () => {
-     cy.visit(url);
+    enterCredentials('invalid-login', 'invalid-password')
     });
   });
 
 describe("Create User with USER role", () => {
   it("should create a new user with USER role and display it in list of users", () => {
-      cy.visit(url);
+      enterCredentials('invalid-login', 'invalid-password')
   });
 });
 
 describe("Create User with incorrect credentials", () => {
   it("should throw an error BAD REQUEST 400 when user sets incorrect password length", () => {
-      cy.visit(url);
+      enterCredentials('invalid-login', 'invalid-password')
   });
 });
 
 describe('Swagger POST API', () => {
   it('should successfully create a new document using post method from swagger', () => {
-    cy.request({
-      method: 'POST',
-      url: 'http://localhost:8080/documents',
-      body: {
-        concreteDocument: {
-          description: 'This is another document',
-          name: 'Tested document 22.05.2023 00:33',
-          userModifiedBy: 13
-        },
-        documentType: 'jpg',
-        parentId: 3,
-        userCreatedById: 13
-      },
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }).then((response) => {
+
       //open the application to make sure that document was created
       cy.visit(url);
+      enterCredentials('invalid-login', 'invalid-password')
     });
   });
 });
