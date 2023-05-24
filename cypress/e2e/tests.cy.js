c14856b2a3940194741adca8389a90449efe1414
katevas <katya.vasilevskaya.01@mail.ru>
2023-05-24 13:01:23 +0300

    CI е2е tests check

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -45,32 +45,36 @@ describe('Export users', () => {
   });
 
   it('should download the list of users after clicking EXPORT button', () => {
+      cy.visit(url);
       enterCredentials('invalid-login', 'invalid-password')
   });
 });
 
 describe("Edit document type", () => {
   it("should edit a document type after clicking EDIT button and display the change", () => {
+      cy.visit(url);
       enterCredentials('invalid-login', 'invalid-password')
   });
 });
 
 describe('Navigate from one page to another', () => {
   it('should navigate from main page to page Catalogues', () => {
-     enterCredentials('invalid-login', 'invalid-password')
+      cy.visit(url);
+      enterCredentials('invalid-login', 'invalid-password')
   })
 })
 
 
 describe('Add new catalogue', () => {
   it('should add new catalogue when user click on button CREATE', () => {
-    enterCredentials('invalid-login', 'invalid-password')
+      cy.visit(url);
+      enterCredentials('invalid-login', 'invalid-password')
   });
 });
 
 describe('Change user role', () => {
   it('should change the user role and display it', () => {
-      enterCredentials('invalid-login', 'invalid-password')
+      cy.visit(url);
       enterCredentials('invalid-login', 'invalid-password')
 
   })
@@ -86,18 +90,21 @@ describe('Export documents info', () => {
   });
 
   it('should download the list of all documents and read the downloaded file', () => {
-    enterCredentials('invalid-login', 'invalid-password')
+      cy.visit(url);
+      enterCredentials('invalid-login', 'invalid-password')
     });
   });
 
 describe("Create User with USER role", () => {
   it("should create a new user with USER role and display it in list of users", () => {
+      cy.visit(url);
       enterCredentials('invalid-login', 'invalid-password')
   });
 });
 
 describe("Create User with incorrect credentials", () => {
   it("should throw an error BAD REQUEST 400 when user sets incorrect password length", () => {
+      cy.visit(url);
       enterCredentials('invalid-login', 'invalid-password')
   });
 });
